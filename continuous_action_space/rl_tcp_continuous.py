2b5f8f35d65fd78be49143cc9aa2b0a1bca62e36
Numair Sani <nsani@u.rochester.edu>
2022-04-18 07:02:40 -0700

    Fix RL state

--- continuous_action_space/rl_tcp_continuous.py
+++ continuous_action_space/rl_tcp_continuous.py
@@ -1,5 +1,4 @@
 from __future__ import division
-import gym
 import numpy as np
 import torch
 from torch.autograd import Variable
@@ -39,7 +38,7 @@ MAX_STEPS = 1000
 MAX_BUFFER = 1000000
 MAX_TOTAL_REWARD = 300
 # Connect the relevant variables here
-S_DIM = 5
+S_DIM = 7
 A_DIM = 1
 A_MAX = 1.8
 
@@ -61,6 +60,9 @@ try:
 		var = Ns3AIRL(1234, rlesc.TcpRlEnv, rlesc.TcpRlAct)
 		#ns3Settings = {'error_p': 1.0}
 		pro = exp.run(show_output=False)
+		to_store = False
+		observation = []
+		state = []
 		while not var.isFinish():
 			with var as data:
 				if not data:
@@ -74,13 +76,22 @@ try:
 				bytesInFlight = data.env.bytesInFlight
 				throughput = data.env.throughput
 				rtt = data.env.rtt
-				print("---------------STATE----------------")
-				print("simTime_us: ", simTime_us)
-				print("ssThres: ", ssThresh, "\n", "cWnd: ", cWnd)
-				print("segmentsAcked: ", segmentsAcked, "\n", "segmentSize: ", segmentSize)
-				print("bytesInFlight: ", bytesInFlight)
-				print("throughput: ", throughput / (2 ** 17), "\n", "rtt: ", rtt / (10 ** 3))
-				print("---------------STATE----------------")
+				new_observation = [ssThresh, cWnd, segmentsAcked, segmentSize, bytesInFlight,
+										throughput, rtt]
+				if to_store and rtt > 0:
+					reward = throughput / rtt
+					print("REWARD: ", reward)
+					r_list.append(reward)
+					new_state = np.float32(new_observation)
+					# push this exp in ram
+					print('------------------------------------')
+					print('State')
+					print(state)
+					print('New State')
+					print(new_state)
+					print('------------------------------------')
+					ram.add(state, action, reward, new_state)
+					trainer.optimize()
 				if args.result:
 					for res in res_list:
 						globals()[res].append(globals()[res[:-2]])
@@ -103,62 +114,17 @@ try:
 					new_ssThresh = int(max(2 * segmentSize, bytesInFlight / 2))
 					data.act.new_cWnd = new_cWnd
 					data.act.new_ssThresh = new_ssThresh
+					to_store = False
 				else:
-					observation = [ssThresh, cWnd, segmentsAcked, segmentSize, bytesInFlight]
+					observation = [ssThresh, cWnd, segmentsAcked, segmentSize, bytesInFlight,\
+												throughput, rtt]
 					state = np.float32(observation)
 					action = trainer.get_exploration_action(state)
 					print('Action:' , action)
 					print(int((2**action)*cWnd))
 					data.act.new_cWnd = int((2**action)*cWnd)
 					data.act.new_ssThresh = int(max(2 * segmentSize, bytesInFlight / 2))
-
-					simTime_us_post = data.env.simTime_us
-					ssThresh_post = data.env.ssThresh
-					cWnd_post = data.env.cWnd
-					segmentsAcked_post = data.env.segmentsAcked
-					segmentSize_post = data.env.segmentSize
-					bytesInFlight_post = data.env.bytesInFlight
-					throughput_post = data.env.throughput
-					rtt_post = data.env.rtt
-
-					if simTime_us != simTime_us_post or ssThresh != ssThresh_post or cWnd != cWnd_post \
-						or segmentsAcked != segmentsAcked_post or segmentSize != segmentSize_post \
-						or bytesInFlight != bytesInFlight_post or throughput != throughput_post or rtt != rtt_post:
-						print("NOT EQUAL")
-
-					new_observation = [ssThresh, cWnd, segmentsAcked, segmentSize, bytesInFlight]
-
-					print("---------STATE AFTER ACTION-----------")
-					print("simTime_us: ", simTime_us_post)
-					print("ssThres: ", ssThresh_post, "\n", "cWnd: ", cWnd_post)
-					print("segmentsAcked: ", segmentsAcked_post, "\n", "segmentSize: ", segmentSize_post)
-					print("bytesInFlight: ", bytesInFlight_post)
-					print("throughput: ", throughput_post / (2 ** 17), "\n", "rtt: ", rtt_post / (10 ** 3))
-					print("---------STATE AFTER ACTION-----------")
-
-					if rtt > 0:
-
-						reward = throughput / rtt
-						print("REWARD: ", reward)
-						r_list.append(reward)
-						s_ = [ssThresh, cWnd, segmentsAcked,
-							  segmentSize, bytesInFlight, throughput, rtt]
-						done = False
-						info = []
-
-						if done:
-							new_state = None
-						else:
-							new_state = np.float32(new_observation)
-							# push this exp in ram
-							ram.add(state, action, reward, new_state)
-						observation = new_observation
-
-					# perform optimization
-					trainer.optimize()
-				if done:
-					break
-
+					to_store = True
 		# check memory consumption and clear memory
 		gc.collect()
 		# process = psutil.Process(os.getpid())
